42ff4efc8a91a1d8b74f4bad16aa6c90157ba533
ep1s0de3 <warfaredev@gmail.com>
2021-05-30 13:41:45 +0400

    Update RNSPerSecondMetrics.cs

--- Assets/RakNet/Enums/RNSPerSecondMetrics.cs
+++ Assets/RakNet/Enums/RNSPerSecondMetrics.cs
@@ -1,14 +1,19 @@
 ﻿public enum RNSPerSecondMetrics
 {
-	/// How many bytes per pushed via a call to send
+	/// How many bytes per pushed via a call to sent
 	USER_MESSAGE_BYTES_PUSHED,
 
-	/// How many user message bytes were sent via a call to send. This is less than or equal to USER_MESSAGE_BYTES_PUSHED.
-	/// A message would be pushed, but not yet sent, due to congestion control
+	/// How many user message bytes were sent
 	USER_MESSAGE_BYTES_SENT,
+
+	/// How many user messages sent
+	USER_MESSAGES_SENT,
 
 	/// How many user message bytes were resent. A message is resent if it is marked as reliable, and either the message didn't arrive or the message ack didn't arrive.
 	USER_MESSAGE_BYTES_RESENT,
+
+	/// How many user messages were resent. A message is resent if it is marked as reliable, and either the message didn't arrive or the message ack didn't arrive.
+	USER_MESSAGES_RESENT,
 
 	/// How many user message bytes were received, and returned to the user successfully.
 	USER_MESSAGE_BYTES_RECEIVED_PROCESSED,
@@ -19,6 +24,12 @@
 	/// How many actual bytes were sent, including per-message and per-datagram overhead, and reliable message acks
 	ACTUAL_BYTES_SENT,
 
+	/// How many actual messages were sent, including per-message and per-datagram overhead, and reliable message acks
+	ACTUAL_MESSAGES_SENT,
+
 	/// How many actual bytes were received, including overead and acks.
-	ACTUAL_BYTES_RECEIVED
+	ACTUAL_BYTES_RECEIVED,
+
+	////// How many actual messages were received, including overead and acks.
+	ACTUAL_MESSAGES_RECEIVED,
 }
