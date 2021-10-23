3a6788584c3d3b1cf6c3e03979c97e6739152f08
Victor <44504047+redheadgektor@users.noreply.github.com>
2021-10-24 02:00:47 +0400

    PlayerLoop changes...
    Now you can shove the RakNet update anywhere...

--- Assets/RakNet/Loop/RakNetLoop.cs
+++ Assets/RakNet/Loop/RakNetLoop.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.LowLevel;
 using UnityEngine.PlayerLoop;
@@ -6,44 +7,18 @@ using UnityEngine.PlayerLoop;
 //   process_packets
 //   all_updates
 //   send_packets
-internal static class RakNetLoop
+internal sealed class RakNetLoop
 {
-    internal enum AddMode { Beginning, End }
-    internal static int FindPlayerLoopEntryIndex(PlayerLoopSystem.UpdateFunction function, PlayerLoopSystem playerLoop, Type playerLoopSystemType)
-    {
-        if (playerLoop.type == playerLoopSystemType)
-            return Array.FindIndex(playerLoop.subSystemList, (elem => elem.updateDelegate == function));
-
-        if (playerLoop.subSystemList != null)
-        {
-            for (int i = 0; i < playerLoop.subSystemList.Length; ++i)
-            {
-                int index = FindPlayerLoopEntryIndex(function, playerLoop.subSystemList[i], playerLoopSystemType);
-                if (index != -1) return index;
-            }
-        }
-        return -1;
-    }
-
-    internal static bool AddToPlayerLoop(PlayerLoopSystem.UpdateFunction function, Type ownerType, ref PlayerLoopSystem playerLoop, Type playerLoopSystemType, AddMode addMode)
+    static bool InsertNewSystem<T>(int index, PlayerLoopSystem.UpdateFunction function, ref PlayerLoopSystem playerLoop, Type playerLoopSystemType)
     {
         if (playerLoop.type == playerLoopSystemType)
         {
-            int oldListLength = (playerLoop.subSystemList != null) ? playerLoop.subSystemList.Length : 0;
-            Array.Resize(ref playerLoop.subSystemList, oldListLength + 1);
-
-            if (addMode == AddMode.Beginning)
-            {
-                Array.Copy(playerLoop.subSystemList, 0, playerLoop.subSystemList, 1, playerLoop.subSystemList.Length - 1);
-                playerLoop.subSystemList[0].type = ownerType;
-                playerLoop.subSystemList[0].updateDelegate = function;
-
-            }
-            else if (addMode == AddMode.End)
-            {
-                playerLoop.subSystemList[oldListLength].type = ownerType;
-                playerLoop.subSystemList[oldListLength].updateDelegate = function;
-            }
+            var list = new List<PlayerLoopSystem>(playerLoop.subSystemList);
+            PlayerLoopSystem sys = new PlayerLoopSystem();
+            sys.type = typeof(T);
+            sys.updateDelegate = function;
+            list.Insert(index, sys);
+            playerLoop.subSystemList = list.ToArray();
             return true;
         }
 
@@ -51,14 +26,14 @@ internal static class RakNetLoop
         {
             for (int i = 0; i < playerLoop.subSystemList.Length; ++i)
             {
-                if (AddToPlayerLoop(function, ownerType, ref playerLoop.subSystemList[i], playerLoopSystemType, addMode))
+                if (InsertNewSystem<T>(index, function, ref playerLoop.subSystemList[i], playerLoopSystemType))
                     return true;
             }
         }
         return false;
     }
 
-    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
     static void Initialize()
     {
 #if UNITY_EDITOR
@@ -75,8 +50,8 @@ internal static class RakNetLoop
         RakServer.Init();
         RakClient.Init();
 
-        PlayerLoopSystem playerLoop = PlayerLoop.GetDefaultPlayerLoop();
-        AddToPlayerLoop(EarlyUpdate, typeof(RakNetLoop), ref playerLoop, typeof(EarlyUpdate), AddMode.End);
+        PlayerLoopSystem playerLoop = PlayerLoop.GetCurrentPlayerLoop();
+        InsertNewSystem<RakNetLoop>(0, Update, ref playerLoop, typeof(EarlyUpdate));
         PlayerLoop.SetPlayerLoop(playerLoop);
 
         Application.quitting += DestroyAll;
@@ -108,7 +83,7 @@ internal static class RakNetLoop
         RakServer.Destroy();
     }
 
-    static void EarlyUpdate()
+    static void Update()
     {
 #if UNITY_EDITOR
         if (!UnityEditor.EditorApplication.isPlaying)
